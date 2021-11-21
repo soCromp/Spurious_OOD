484b1661c80d34ec21386451832b890a0fcb2af1
soCromp <snc40@pitt.edu>
2021-11-20 18:10:54 -0600

    new branch

--- runexp.sh
+++ runexp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-name=2021-11-12_18.39.23
-# `date +"%Y-%m-%d_%H.%M.%S"`
+name=`date +"%Y-%m-%d_%H.%M.%S"`
 data=$1
 corr=$2
 gpu=$3
@@ -13,7 +12,7 @@ ENV=(
 
 echo $name
 echo "Train"
-# python train_bg.py --gpu-ids $gpu --in-dataset $data --model resnet18 --epochs 30 --save-epoch 30 --data_label_correlation $corr --domain-num 4 --method erm --name $name  --lr 0.001 --weight-decay 0.001
+python train_bg.py --gpu-ids $gpu --in-dataset $data --model resnet18 --epochs 30 --save-epoch 30 --data_label_correlation $corr --domain-num 4 --method erm --name $name  --lr 0.001 --weight-decay 0.001
 # echo "Get activations"
 # python get_activations.py --gpu-ids $gpu --in-dataset $data --model resnet18 --test_epochs 30 --data_label_correlation $corr --method erm --name $name  --root_dir datasets/ood_datasets 
 # python get_activations.py --gpu-ids $gpu --in-dataset $data --model resnet18 --test_epochs 30 --data_label_correlation $corr --method erm --name $name  --root_dir datasets/ood_datasets --datasplit test
@@ -22,11 +21,11 @@ echo "Train"
 # python compare_group_activations.py $data $name
 
 # echo "Test all units"
-# python test_bg.py --gpu-ids $gpu --in-dataset $data --model resnet18 --test_epochs 30 --data_label_correlation $corr --method erm --name $name  --root_dir datasets/ood_datasets -cm 0
+python test_bg.py --gpu-ids $gpu --in-dataset $data --model resnet18 --test_epochs 30 --data_label_correlation $corr --method erm --name $name  --root_dir datasets/ood_datasets -cm 0
 # echo "Present results"
-# python present_results.py --in-dataset $data --name $name  --test_epochs 30 -cm 0
+python present_results.py --in-dataset $data --name $name  --test_epochs 30 -cm 0
 
-python plot_energy.py $name $data
+# python plot_energy.py $name $data
 
 # for e in "${ENV[@]}"; do
 #     echo "Test 0-100, top 200"
